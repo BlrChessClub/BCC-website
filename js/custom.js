0932a2c459f5c0c198e9ffddac2640ded9f7e8cf
Nemo <me@captnemo.in>
2024-07-19 18:22:02 +0530

    drop googlemap and popper
    no tooltips used, so drop popper
    the google map key used returns a BillingNotEnabledError +
    the googleMap div isn't used anywhere

--- js/custom.js
+++ js/custom.js
@@ -35,17 +35,6 @@ $(".client_owl-carousel").owlCarousel({
     }
 });
 
-
-
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-
 // Get the navigation bar
 var navBar = document.querySelector('.custom_nav-container');
 
